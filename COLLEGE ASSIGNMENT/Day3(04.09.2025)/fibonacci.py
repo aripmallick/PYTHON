fc1fafe2bccb8f7af6b983ae587c76025fe306c3
Arip Mallick <aripmallick@gmail.com>
2025-09-09 00:00:21 +0530

    edited

--- COLLEGE ASSIGNMENT/Day3(04.09.2025)/fibonacci.py
+++ COLLEGE ASSIGNMENT/Day3(04.09.2025)/fibonacci.py
@@ -1,12 +1,38 @@
+# Fibonacci Series without function
+
 n=int(input("Enter a number: "))
 
-a,b=0,1
 
-while b<n :
-    a,b=b,a+b
-if b==n or n==0 :
-    print(n,"is a fibonacci number")
-else :
-    print(n,"is not a fibonacci number")
+a, b = 0, 1
+odd = []
+even = []
+
+print("Fibonacci series:")
+
+for i in range(n):
+    print(a, end=" ")
+
+    if a % 2 == 0:
+        even.append(a)
+    else:
+        odd.append(a)
+
+    a, b = b, a + b
+
+print("\n\nOdd Fibonacci numbers:", odd)
+print("Even Fibonacci numbers:", even)
+
+
+
+#n=int(input("Enter a number: "))
+#
+#a,b=0,1
+#
+#while b<n :
+#    a,b=b,a+b
+#if b==n or n==0 :
+#    print(n,"is a fibonacci number")
+#else :
+#    print(n,"is not a fibonacci number")
 
 #Example: 0 + 1 = 1, 1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8, 5 + 8 = 13
--- COLLEGE ASSIGNMENT/Day3(04.09.2025)/reverse.py
+++ COLLEGE ASSIGNMENT/Day3(04.09.2025)/reverse.py
@@ -1,10 +1,10 @@
 a=int(input("Enter a integer: "))
-sign=-1 if a<0 else 1
-a=abs(a)
+#sign=-1 if a<0 else 1
+#a=abs(a)
 reverse_num=0
 while a>0:
     digit=a%10
     reverse_num=reverse_num*10+digit
     a//=10
-reverse_num*=sign
+#reverse_num*=sign
 print("Reversed integer: ",reverse_num)
